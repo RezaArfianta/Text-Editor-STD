9559db027c94bb251639c205fb787ea8adfc6630
Rafi Arkan Fachreza Arfianta <90881351+RezaArfianta@users.noreply.github.com>
2024-12-02 22:59:52 +0700

    added menu

---
+++ MENU/functionMenu.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "headerMenu.h"
+using namespace std;
+
+void menu()
+{
+    cout << "1. Tambah Kalimat" << endl;
+    cout << "2. Pilih Kalimat" << endl;
+    cout << "3. Tutup Aplikasi" << endl;
+    cout << "Pilih opsi: ";
+}
